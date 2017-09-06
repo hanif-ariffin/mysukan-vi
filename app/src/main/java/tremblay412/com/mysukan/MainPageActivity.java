b0a1ca968f104ed50103be5a7444e454a1638888
HAZIQ <haziq_yusof@hotmail.com>
2017-09-06 12:24:24 -0400

    Registration Page Fixed 2.0

--- app/src/main/java/tremblay412/com/mysukan/MainPageActivity.java
+++ app/src/main/java/tremblay412/com/mysukan/MainPageActivity.java
@@ -1,14 +1,18 @@
 package tremblay412.com.mysukan;
 
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ArrayAdapter;
 
+import android.widget.Button;
 import android.widget.ListView;
 
 
@@ -25,12 +29,24 @@ public class MainPageActivity extends AppCompatActivity{
     // UI references.
     private ListView iListGames;
     private ViewGroup iMainPage;
-
+    private FloatingActionButton RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+
+        // Open Registration Page
+        RegisterButton = (FloatingActionButton)findViewById(R.id.RegisterButton);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainPageActivity.this,RegisterActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
 
         //mMainPage = findViewById(R.layout.main_page);
         iMainPage = (ViewGroup) findViewById(R.id.main_page);
