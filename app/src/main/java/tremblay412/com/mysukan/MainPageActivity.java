1de5e5ec099a44aa027b51ff123dd474222bfbc5
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2017-09-07 16:27:31 -0400

    converted MainActivity to extend Fragment instead of class

--- app/src/main/java/tremblay412/com/mysukan/MainPageActivity.java
+++ app/src/main/java/tremblay412/com/mysukan/MainPageActivity.java
@@ -46,11 +46,13 @@ public class MainPageActivity extends AppCompatActivity {
         mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             private final Fragment[] mFragments = new Fragment[]{
                     new LoginFragment(),
-                    new AdminFragment()
+                    new AdminFragment(),
+                    new SportFragment()
             };
             private final String[] mFragmentNames = new String[]{
                     "Login",
-                    "Admin"
+                    "Admin",
+                    "SportFragment"
             };
 
             @Override
@@ -77,44 +79,7 @@ public class MainPageActivity extends AppCompatActivity {
         /**
          *
          * OLD ACTIVITY
-         super.onCreate(savedInstanceState);
-         setContentView(R.layout.main_page);
-
-         // Open Registration Page
-         RegisterButton = (FloatingActionButton) findViewById(R.id.RegisterButton);
-         RegisterButton.setOnClickListener(new View.OnClickListener() {
-        @Override public void onClick(View view) {
-        Intent myIntent = new Intent(MainPageActivity.this, LoginFragment.class);
-        startActivity(myIntent);
-        }
-        });
-
-
-         //mMainPage = findViewById(R.layout.main_page);
-         iMainPage = (ViewGroup) findViewById(R.id.main_page);
-
-         iListGames = (ListView) findViewById(R.id.listview1);
-
-         ArrayAdapter<String> lArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getGames());
-         iListGames.setAdapter(lArrayAdapter);
          **/
-    }
-
-    private List<String> getGames() {
-        List<String> lGames = new ArrayList<>();
-        lGames.add("Volleyball");
-        lGames.add("Telematch (F)");
-        lGames.add("Dodgeball");
-        lGames.add("Frisbee");
-        lGames.add("Badminton");
-        lGames.add("Squash");
-        lGames.add("Volleyball (F)");
-        lGames.add("Netball");
-        lGames.add("Basketball");
-        lGames.add("Football");
-        lGames.add("Fifa (E-Games)");
-        lGames.add("Rocket League (E-games)");
-        return lGames;
     }
 }
 
