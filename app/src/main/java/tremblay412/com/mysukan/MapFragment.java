d4a5f96971c7b614d644b5d59f2dff39d6a66eaf
HafizGhani <hafizghani.pigoh@gmail.com>
2017-09-08 05:19:21 -0400

    add google map to the apps

--- app/src/main/java/tremblay412/com/mysukan/MapFragment.java
+++ app/src/main/java/tremblay412/com/mysukan/MapFragment.java
@@ -1,5 +1,6 @@
 package tremblay412.com.mysukan;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,16 +9,41 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by User on 2017-09-07.
  */
 
-public class MapFragment extends BaseFragment {
+public class MapFragment extends BaseFragment implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.map_fragment, container, false);
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
         return rootView;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng carletonU = new LatLng(45.3876,-75.6960);
+        mMap.addMarker(new MarkerOptions().position(carletonU).title("Carleton University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(carletonU));
+    }
+
 }
