b421632246bda33820c57572a15042231b081813
hanif-ariffin <hanif.ariffin.4326@gmail.com>
2017-10-02 16:03:01 -0400

    refactored and organized NameManager, preparing to create OverralWinnerActivity

--- app/src/main/java/tremblay412/com/mysukan/activities/OverallWinnerActivity.java
+++ app/src/main/java/tremblay412/com/mysukan/activities/OverallWinnerActivity.java
@@ -1,8 +1,13 @@
 package tremblay412.com.mysukan.activities;
 
+import com.google.firebase.database.DatabaseReference;
+
 /**
  * Created by Akarin on 10/1/2017.
  */
 
 public class OverallWinnerActivity extends BaseActivity {
+
+
+    private DatabaseReference sportNameReference;
 }
